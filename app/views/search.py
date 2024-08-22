a6bb83f2f91e8c211e4d8781506602cb01461b49
sleeping <hh.sleeping@gmail.com>
2024-08-22 18:27:29 +0400

    geocoding: send user lang to nominatim

--- app/views/search.py
+++ app/views/search.py
@@ -21,11 +21,14 @@ async def get_nominatim_response_from_cache(conn, query: str) -> dict:
         query)
 
 
-async def search_locations(query: str) -> dict:
+async def search_locations(query: str, lang: str) -> dict:
     '''
-    search location in nominatim addresses.
+    search location in nominatim.
     returns results as FeatureCollection - each location is geojson with properties, bbox and geometry
     '''
+    url = f'search?q={query}&format=geojson&polygon_geojson=1'
+    if lang:
+        url += f'&accept-language={lang}'
     conn = await get_db_conn()
     await conn.set_type_codec(
         'jsonb',
@@ -33,7 +36,7 @@ async def search_locations(query: str) -> dict:
         decoder=json.loads,
         schema='pg_catalog',
     )
-    if result := await get_nominatim_response_from_cache(conn, query):
+    if result := await get_nominatim_response_from_cache(conn, url):
         await conn.close()
         LOGGER.debug('found response in cache')
         return result
@@ -43,26 +46,26 @@ async def search_locations(query: str) -> dict:
     try:
         await conn.execute(
             'insert into nominatim_cache (query, response) values ($1, $2)',
-            query, None,
+            url, None,
         )
     except asyncpg.exceptions.UniqueViolationError:
         # other transaction saved nominatim response first, return it
         await tr.rollback()
         LOGGER.debug('return nominatim response committed by other transaction')
-        result = await get_nominatim_response_from_cache(conn, query)
+        result = await get_nominatim_response_from_cache(conn, url)
         await conn.close()
         return result
 
     async with ClientSession() as session:
-        async with session.get(f'https://nominatim.openstreetmap.org/search?q={query}&format=geojson&layer=address&polygon_geojson=1') as response:
+        async with session.get('https://nominatim.openstreetmap.org/' + url) as response:
             nominatim_response = await response.json()
     await conn.execute(
         'update nominatim_cache set response = $1 where query = $2',
-        nominatim_response, query,
+        nominatim_response, url,
     )
     await tr.commit()
     await conn.close()
-    LOGGER.debug('saved nominatim response for query = %s', query)
+    LOGGER.debug('saved nominatim response for query = %s', url)
     return nominatim_response
 
 
@@ -83,10 +86,11 @@ async def search(request: 'Request') -> 'Response':
     if not (query := data.get('query').strip()):
         raise HTTPException(status_code=400, detail='missing query')
 
+    lang = request.headers.get('User-Language')
     search_results = {}
 
     user_data = await get_user_data(app_id, auth_token=request.headers.get('Authorization'))
     if feature_enabled('search_locations', user_data):
-        search_results['locations'] = await search_locations(query)
+        search_results['locations'] = await search_locations(query, lang)
 
     return JSONResponse(search_results)
